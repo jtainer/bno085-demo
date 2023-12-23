83cb1e165c1f166b43843f7501184dbe604da8df
jtainer <92753845+jtainer@users.noreply.github.com>
2023-12-22 19:07:48 -0500

    Add files via upload

---
+++ lib/entry/entry.c
@@ -0,0 +1,13 @@
+// 
+// Entry point for user code
+// 'setup' is called once after device reset
+// 'loop' is called repeatedly
+//
+// Copyright (c) 2023 Jonathan Tainer
+//
+
+void setup(void) {
+}
+
+void loop(void) {
+}
